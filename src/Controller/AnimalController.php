d3457407084e81f2f9761dae101e02be2337ca8b
Rodolphe <dutchr@live.fr>
2021-07-27 10:36:06 +0200

    code refactor for optimisation

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Animal;
+use App\Repository\AnimalRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,9 +11,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AnimalController extends AbstractController
 {
     #[Route('/', name: 'animaux')]
-    public function index(): Response
+    public function index(AnimalRepository $repository): Response
     {
-      $repository = $this->getDoctrine()->getRepository(Animal::class);
       $animaux = $repository->findAll();
         return $this->render('animal/index.html.twig',[
           "animaux" => $animaux
