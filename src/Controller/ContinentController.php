703fd030da4436143c06ebabe1e08005d30b2c8e
Rodolphe <dutchr@live.fr>
2021-07-27 16:59:04 +0200

    WIP: DIsplay continents on All pages with animals

--- src/Controller/ContinentController.php
+++ src/Controller/ContinentController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Continent;
+use App\Repository\ContinentRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContinentController extends AbstractController
 {
     #[Route('/continent', name: 'continent')]
-    public function index(): Response
+    public function index(ContinentRepository $repository): Response
     {
+      $continents = $repository->findAll();
         return $this->render('continent/index.html.twig', [
-            'controller_name' => 'ContinentController',
+            'continents' => $continents,
         ]);
+    }
+    #[Route('/continent/{id}', name: 'display_continent')]
+    public function afficherContinent(Continent $continent)
+    {
+      return $this->render('continent/afficherContinent.html.twig',[
+        'continent' => $continent,
+      ]);
     }
 }
