3596665de7f5f52714da56192b204f694875c393
Rodolphe <dutchr@live.fr>
2021-07-27 10:33:55 +0200

    Display data & fix style

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Animal;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AnimalController extends AbstractController
 {
     #[Route('/', name: 'animaux')]
     public function index(): Response
     {
-        return $this->render('animal/index.html.twig');
+      $repository = $this->getDoctrine()->getRepository(Animal::class);
+      $animaux = $repository->findAll();
+        return $this->render('animal/index.html.twig',[
+          "animaux" => $animaux
+        ]);
     }
 }
